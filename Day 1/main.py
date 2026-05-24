4518cfea061dbfe702abe9235830e5f9594e881a
sravanbommideni <sharavan.b.jmkt@gmail.com>
2026-05-24 19:05:38 +0530

    Tracking daily progress

---
+++ Day 1/main.py
@@ -0,0 +1,3 @@
+city_name = input("Enter the city name you grew up in: ")
+pet_name = input("Enter the name of your pet: ")
+print("""your band would be \"""" + city_name + """ """ + pet_name + """\"""")
