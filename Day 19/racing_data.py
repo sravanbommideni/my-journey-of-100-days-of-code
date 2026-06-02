392e83b8470b6e9846eb03c086369a3dfd02f75d
sravanbommideni <sharavan.b.jmkt@gmail.com>
2026-06-02 16:04:54 +0530

    added data for turtle racing game

---
+++ Day 19/racing_data.py
@@ -0,0 +1,26 @@
+objects_data = [
+    {
+        "name" : "inky",
+        "color" : "black"
+    },
+    {
+        "name" : "pinky",
+        "color" : "grey"
+    },
+    {
+        "name" : "ponky",
+        "color" : "blue"
+    },
+    {
+        "name" : "father",
+        "color" : "red"
+    },
+    {
+        "name" : "donkey",
+        "color" : "violet"
+    },
+    {
+        "name" : "junky",
+        "color" : "green"
+    }
+]
